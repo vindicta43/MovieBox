2fd025170a627bbb60394bb2d9d462c44f5072e3
Alperen Erdoğan <alperen.erdogan.neu@gmail.com>
2021-08-26 10:43:57 +0300

    initial commit and splash screen

---
+++ app/src/main/java/com/alperen/moviebox/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alperen.moviebox.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alperen.moviebox.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
